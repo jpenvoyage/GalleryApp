45f40967e764a44949da72561ebd41c1e9183c04
joaobug <joaoenfrance@gmail.com>
2024-01-24 18:58:16 -0300

    Initial Commit

---
+++ GalleryApp/GalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryAppApp.swift
+//  GalleryApp
+//
+//  Created by Joao Leal on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct GalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
